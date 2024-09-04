6458f883443652957e0e7d21cdea3738aef4c3d0
mariopeluso <mariopeluso00@gmail.com>
2024-09-04 17:38:11 +0200

    test(front-end): add test for reportStory method

--- refair-client/src/components/__tests__/refair.spec.js
+++ refair-client/src/components/__tests__/refair.spec.js
@@ -1,12 +1,49 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, it, vi, beforeEach,expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import refair from '../refair.vue'
+import axios from "axios";
+
+
+vi.mock('axios')
+const mockDownloadjs = vi.fn();
+global.dowloadjs = mockDownloadjs;
 
 describe('refair.vue', () => {
-  it('should mount the component and check absence of non-existent element (EXAMPLE)', () => {
-    const wrapper = mount(refair)
-    expect(wrapper.exists()).toBe(true)
-    expect(wrapper.find('does-not-exist').exists()).toBe(false)
-  })
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(refair, {
+      data() {
+        return {
+          file: null,
+          stories: [],
+          story: '',
+          story_domain: '',
+          story_tasks: [],
+          activeAnalyzeStoryModal: false,
+          series: [{ name: 'series-1', data: [] }],
+        };
+      },
+    });
+  });
+
+  it('reportStory', async () => {
+    axios.post.mockResolvedValue({
+      data: "{'story': 'report'}",
+    });
+
+    await wrapper.vm.reportStory();
+
+    expect(axios.post).toHaveBeenCalledWith(
+      expect.stringContaining('/reportStory'),
+      expect.any(FormData),
+      { headers: { 'Content-Type': 'multipart/form-data' } }
+    );
+
+    expect(mockDownloadjs).toHaveBeenCalledWith(
+      '{"story":"report"}',
+      'report-example-story.json',
+      'application/json'
+    );
+  });
 })
